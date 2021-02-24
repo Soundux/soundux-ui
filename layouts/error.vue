ccede50a8c928d2f02fd360c81b840cd74484f6e
D3SOX <d3sox@protonmail.com>
2021-02-24 14:51:29 +0100

    feat: improve error layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,21 +1,32 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
+    <v-main>
+      <v-container>
+        <v-card>
+          <v-card-title v-if="error.statusCode === 404">
+            {{ pageNotFound }}
+          </v-card-title>
+          <v-card-title v-else>
+            {{ otherError }}
+          </v-card-title>
+          <v-card-actions>
+            <NuxtLink to="/">Home page</NuxtLink>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import { NuxtError } from '@nuxt/types';
+
+export default Vue.extend({
   layout: 'empty',
   props: {
     error: {
-      type: Object,
+      type: Object as PropType<NuxtError>,
       default: null,
     },
   },
@@ -25,17 +36,13 @@ export default {
       otherError: 'An error occurred',
     };
   },
-  head() {
+  head(): object {
     const title = this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
     return {
       title,
     };
   },
-};
+});
 </script>
 
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
+<style scoped></style>
