ee4e8fde484cfd313100af1a330f68be3606cf0b
D3SOX <d3sox@protonmail.com>
2021-02-26 14:31:24 +0100

    chore: re-add vuetify eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,15 @@ module.exports = {
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
   ],
+  plugins: ['vuetify'],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vuetify/no-deprecated-classes': 'error',
+    'vuetify/no-deprecated-components': 'error',
     '@typescript-eslint/ban-ts-comment': 'off',
   },
 };
