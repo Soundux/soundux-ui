02ab9834bdc9df6cba703adfd9d6dabde182e0f4
D3SOX <d3sox@protonmail.com>
2021-03-03 20:16:05 +0100

    refactor: translations as json

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,13 +4,13 @@ import VueI18n, { LocaleMessages } from 'vue-i18n';
 Vue.use(VueI18n);
 
 function loadLocaleMessages(): LocaleMessages {
-  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.js$/i);
+  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i);
   const messages: LocaleMessages = {};
   locales.keys().forEach(key => {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
     if (matched && matched.length > 1) {
       const locale = matched[1];
-      messages[locale] = locales(key).default;
+      messages[locale] = locales(key);
     }
   });
   return messages;
