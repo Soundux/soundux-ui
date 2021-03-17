03f4cfd3cb9103479901ff44143d51822cdc7055
Nico <d3sox@protonmail.com>
2021-03-17 16:31:18 -0700

    fix: application is not set on windows

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,8 @@ import { Output } from '@/types';
 
 export function getPrettyName(output: Output): string {
   const { name, application } = output;
+  if (!application) {
+    return name;
+  }
   return name.toLowerCase() === application.toLowerCase() ? application : `${name} (${application})`;
 }
